01a3dab49f6951e37908c2266aa9b0a6984997b0
3mm3rmo7amed-AMR11 <ammar132773@gmail.com>
2026-02-15 15:59:08 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+let dark = true;
+
+function welcomeUser() {
+ 
+    const nameInput = document.getElementById("username");
+    const welcomeText = document.getElementById("welcome");
+    
+    if (nameInput.value === "") {
+        welcomeText.innerText = "Please write your name!";
+    } else {
+        welcomeText.innerText = "Welcome, " + nameInput.value + "👋";
+    }
+}
+
+function toggleMode() {
+    const body = document.body;
+    
+    if (dark) {
+        body.style.backgroundColor = "white";
+        body.style.color = "black";
+        dark = false;
+    } else {
+        body.style.backgroundColor = "black";
+        body.style.color = "white";
+        dark = true;
+    }
+}
